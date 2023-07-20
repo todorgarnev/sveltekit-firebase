c18e65b6b85dd2976e541f3f432ef54714255a79
Todor Garnev <tgarnev@abv.bg>
2023-07-20 16:26:28 +0300

    Added firebase google login/logout

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyCXGlW0nrmhV_vIC8kCqKkGW0PzMhmTIpc",
@@ -18,3 +19,33 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+/**
+ * @returns a store with the current firebase user
+ */
+const userStore = () => {
+	let unsubscribe: () => void;
+
+	if (!auth || !globalThis.window) {
+		console.warn("Auth is not initialized or not in browser");
+		const { subscribe } = writable(null);
+
+		return {
+			subscribe
+		};
+	}
+
+	const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+		unsubscribe = onAuthStateChanged(auth, (user) => {
+			set(user);
+		});
+
+		return () => unsubscribe();
+	});
+
+	return {
+		subscribe
+	};
+};
+
+export const user = userStore();
