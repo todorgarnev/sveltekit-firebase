d416d388fade2af1733c0596f1c6b18ba0824b0f
Todor Garnev <tgarnev@abv.bg>
2023-07-21 10:35:29 +0300

    added usernames connection to the firebase

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,5 +1,41 @@
 <script lang="ts">
+	import AuthCheck from "$lib/components/AuthCheck.svelte";
+	import { db, user } from "$lib/firebase";
+	import { doc, getDoc, writeBatch } from "firebase/firestore";
 
+	let username: string = "";
+	let loading: boolean = false;
+	let isAvailable: boolean = false;
+	let debounceTimer: NodeJS.Timeout;
+
+	const checkAvailability = async () => {
+		isAvailable = false;
+		clearTimeout(debounceTimer);
+
+		loading = true;
+
+		debounceTimer = setTimeout(async () => {
+			const ref = doc(db, "usernames", username);
+			const exists = await getDoc(ref).then((doc) => doc.exists());
+
+			isAvailable = !exists;
+			loading = false;
+		}, 500);
+	};
+
+	const confirmUsername = async () => {
+		// TODO
+	};
 </script>
 
-<div>Username</div>
+<AuthCheck>
+	<h2>Username</h2>
+
+	<form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+		<input type="text" placeholder="Username" class="w-full input" bind:value={username} on:input={checkAvailability} />
+
+		<p>Is available? {isAvailable}</p>
+
+		<button class="btn btn-success">Confirm username @{username} </button>
+	</form>
+</AuthCheck>
