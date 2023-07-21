df3b2dd29322e56cca602e86d048e8bf5ef97adc
Todor Garnev <tgarnev@abv.bg>
2023-07-21 15:46:08 +0300

    added update bio logic

--- src/routes/[username]/bio/+page.server.ts
+++ src/routes/[username]/bio/+page.server.ts
@@ -1,4 +1,4 @@
-import { error, redirect } from "@sveltejs/kit";
+import { error, fail, redirect } from "@sveltejs/kit";
 import { adminDB } from "$lib/server/admin";
 
 export const load = async ({ locals, params }) => {
@@ -18,4 +18,26 @@ export const load = async ({ locals, params }) => {
 	return {
 		bio
 	};
+};
+
+export const actions = {
+	default: async ({ locals, request, params }) => {
+		const uid = locals.userID;
+		const data = await request.formData();
+		const bio = data.get("bio");
+		const userRef = adminDB.collection("users").doc(uid!);
+		const { username } = (await userRef.get()).data()!;
+
+		if (params.username !== username) {
+			throw error(401, "That username does not belong to you");
+		}
+
+		if (bio!.length > 260) {
+			return fail(400, { problem: "Bio must be less than 260 characters" });
+		}
+
+		await userRef.update({
+			bio
+		});
+	}
 };
